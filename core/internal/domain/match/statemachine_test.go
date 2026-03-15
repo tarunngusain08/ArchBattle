1bb486d4aff3d276d3912754769d816836cf1a63
tarunngusain08 <prudent.tarun0808@gmail.com>
2026-03-15 09:25:49 +0530

    adding the UI and backend changes: statemachine_test.go

---
+++ core/internal/domain/match/statemachine_test.go
@@ -0,0 +1,45 @@
+package match
+
+import "testing"
+
+func TestCanTransition(t *testing.T) {
+	valid := [][2]MatchState{
+		{StateCreated, StateLobby},
+		{StateLobby, StateActive},
+		{StateLobby, StateAbandoned},
+		{StateActive, StateRevealing},
+		{StateActive, StateAbandoned},
+		{StateRevealing, StateActive},
+		{StateRevealing, StateScoring},
+		{StateScoring, StateEnded},
+	}
+	for _, pair := range valid {
+		if !CanTransition(pair[0], pair[1]) {
+			t.Errorf("expected CanTransition(%s, %s) = true", pair[0], pair[1])
+		}
+	}
+
+	invalid := [][2]MatchState{
+		{StateCreated, StateActive},
+		{StateActive, StateEnded},
+		{StateEnded, StateLobby},
+		{StateAbandoned, StateActive},
+	}
+	for _, pair := range invalid {
+		if CanTransition(pair[0], pair[1]) {
+			t.Errorf("expected CanTransition(%s, %s) = false", pair[0], pair[1])
+		}
+	}
+}
+
+func TestMustTransition(t *testing.T) {
+	if err := MustTransition(StateCreated, StateCreated); err != nil {
+		t.Errorf("same state should be allowed: %v", err)
+	}
+	if err := MustTransition(StateCreated, StateLobby); err != nil {
+		t.Errorf("valid transition should not error: %v", err)
+	}
+	if err := MustTransition(StateEnded, StateLobby); err == nil {
+		t.Error("invalid transition should return error")
+	}
+}
